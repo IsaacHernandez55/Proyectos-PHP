17e8055bade52a7c5349c7ed6e08e95ebb0f6962
IsaacHernandez55 <30442740+IsaacHernandez55@users.noreply.github.com>
2017-08-09 11:19:36 -0500

    Add files via upload

--- Ejercicio1.php
+++ Ejercicio1.php
@@ -2,31 +2,44 @@
 <html>
   <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
     <title>Ejercicio 1</title>
 
-    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
     <link rel="stylesheet" href="font-awesome/css/font-awesome.min.css">
 
   </head>
   <body>
+
     <div class="container">
+      <h2>Ejercicios For</h2>
+      <form method="post">
+        <label>Ingres un numero: &nbsp &nbsp &nbsp</label>
+        <input type="number" name="num1" value="">
+        <br>
+        <label>Ingrese otro numero: &nbsp</label>
+        <input type="number" name="num2" value="">
+        <br>
+        <input class="btn btn-target" type="submit" name="Enviar" value="Calcular">
+        <hr>
+      </form>
 
-      <h2>Estructura de control Iteractiva</h2>
-      <h3>Muestre los numeros del 1 al 100</h3>
-      <hr>
       <?php
-        $n=1;
-        while ($n <= 100) {
-          echo "$n &nbsp";
-          $n++;
-        }
+      $num1 = $_POST['num1'];
+      $num2 = $_POST['num2'];
 
+      echo "Los numeros pares que hay del primero al segundo son: ";
+      for ($num1 = $num1; $num1 <= $num2 ; $num1++) {
+        if (($num1%2)==0) {
+          echo "$num1 &nbsp";
+        }
+      }
        ?>
        <hr>
        <a class="btn btn-danger"href="index.php"><i class="fa fa-times fa-1x" aria-hidden="true"></i></a>
-    </div>
 
+    </div>
     <script src="js/jquery.js"></script>
     <script src="js/bootstrap.min.js"></script>
   </body>
--- Ejercicio6.php
+++ Ejercicio6.php
@@ -2,39 +2,42 @@
 <html>
   <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <title>Ejercicio 6</title>
 
-    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
     <link rel="stylesheet" href="font-awesome/css/font-awesome.min.css">
-
 
   </head>
   <body>
+
     <div class="container">
+      <h2>Ejercicios For</h2>
+      <form method="post">
+        <label>Ingres un numero: &nbsp &nbsp &nbsp</label>
+        <input type="number" name="num1" value="">
+        <br>
+        <input class="btn btn-target" type="submit" name="Enviar" value="Calcular">
+        <hr>
+      </form>
 
-      <h2>Estructura de control Iteractiva</h2>
-      <h3>Suma de todos los numeros del 1 al 100</h3>
-      <hr>
       <?php
-        $n=1;
-        $suma=0;
-        while ($n <= 100) {
-          $suma=$suma+$n;
-          $n++;
+      $num1 = $_POST['num1'];
 
-        }
+      echo "Tabla de Multplicar del numero $num1 <br>";
 
-        echo "<h2>La suma de todos los numero es: $suma <br/></h2>";
+      for ($i=0; $i <=12 ; $i++) {
+        echo "$num1 x $i = ".($num1*$i)."<br>";
+      }
+
 
        ?>
        <hr>
        <a class="btn btn-danger"href="index.php"><i class="fa fa-times fa-1x" aria-hidden="true"></i></a>
-    </div>
-    
-     <script src="js/jquery.js"></script>
-     <script src="js/bootstrap.min.js"></script>
 
+    </div>
+    <script src="js/jquery.js"></script>
+    <script src="js/bootstrap.min.js"></script>
   </body>
 </html>
